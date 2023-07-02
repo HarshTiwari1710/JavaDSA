1eca61d2410304303c2241e2b2f4be6b861640d6
harshtiwari1710 <2019644021.harsh@ug.sharda.ac.in>
2023-07-02 18:33:05 +0530

    This is code till opreators

---
+++ operators.java
@@ -0,0 +1,16 @@
+public class operators {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 123;
+        //Arithematic operator
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 % num2);
+        System.out.println(++num1);
+        System.out.println(num1++);
+        System.out.println(num2--);
+        System.out.println(--num2);
+    }
+}
---
+++ varandt.java
@@ -0,0 +1,15 @@
+public class varandt {
+    public static void main(String[] args) {
+        //variables and data type
+        int age = 18;
+        float avgMarks = 10.0f;
+        char grade = 'A';
+        String name = "John";
+        boolean isvalid = false;
+        System.out.println(age);
+        System.out.println(avgMarks);
+        System.out.println(grade);
+        System.out.println(name);
+        System.out.println(isvalid);
+    }
+}
