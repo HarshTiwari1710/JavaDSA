2337b103ac78ec4563c43660b024e652113f5a27
harshtiwari1710 <2019644021.harsh@ug.sharda.ac.in>
2023-07-03 22:07:51 +0530

    Day2

---
+++ ConditionalOperator.java
@@ -0,0 +1,11 @@
+public class ConditionalOperator {
+  public static void main(String[] args) {
+    int age = 12;
+    if(age >=18){
+        System.out.println("You can vote");
+    }
+    else if(age< 18) {
+        System.out.println("You cannot vote");
+    }
+  }  
+}
